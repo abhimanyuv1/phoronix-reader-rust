a3e43b82e3b7deaf9e1c0c31e58ee09d7c64c022
Abhimanyu V <abhimanyu.v@gmail.com>
2021-07-18 11:58:20 +0530

    Get html directly from phoronix website
    Signed-off-by: Abhimanyu V <abhimanyu.v@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,32 @@
-extern crate hyper;
+extern crate ureq;
 extern crate select;
 extern crate colored;
 
+// select
 use select::document::Document;
 use select::predicate::{Class,Name};
 use select::node::Node;
-
+// colored
 use colored::*;
 
 fn main() {
     let phoronix_articles = Article::get_articles();
     for article in phoronix_articles {
         println!("title: {}", article.title.red().bold());
-        println!("link: {}", article.link.blue().underline());
+        println!("link: {}{}", "https://www.phoronix.com/".blue().underline(), article.link.blue().underline());
         println!("details: {}", article.details.cyan());
         println!("summary: {}\n", article.summary.green());
     }
 }
 
+#[allow(dead_code)]
 fn open_testing() -> &'static str {
     include_str!("phoronix.html")
+}
+
+fn get_phoronix_page() -> Result<String, ureq::Error> {
+    let body = ureq::get("https://www.phoronix.com/").call()?.into_string()?;
+    Ok(body)
 }
 
 struct Article {
@@ -31,10 +38,11 @@ struct Article {
 
 impl Article {
     fn get_articles() -> Vec<Article> {
-        Document::from(open_testing())
-            .find(Name("article"))
-            .map(|node| Article::new(&node))
-            .collect()
+        let data = get_phoronix_page().unwrap();
+            Document::from(data.as_str())
+                .find(Name("article"))
+                .map(|node| Article::new(&node))
+                .collect()
     }
 
     fn new(node: &Node) -> Article {
